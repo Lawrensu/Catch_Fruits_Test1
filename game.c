06a562bdd016c7f78f64403838a5412c029ad0cf
Lawrence Lian <LawrensuLeo@gmail.com>
2024-12-06 13:52:24 +0800

    Added more stuff

--- game.c
+++ game.c
@@ -4,53 +4,56 @@
 #include <time.h>   // For time()
 #include <string.h> // For strlen()
 
+typedef enum {
+    MENU,
+    GAME,
+    INFO,
+    GAME_OVER
+} GameState;
+
 int main()
 {
-    Music music;
-    Texture2D menuPic;
-    Sound bruh;
-    Sound chatSound;
+    Music bgMusic;
+    Texture2D menuBackground;
+    Music menuMusic;
     
     // Initialization
     const int screenWidth = 1200;
     const int screenHeight = 800;
     
-    InitWindow(screenWidth, screenHeight, "Test");
+    InitWindow(screenWidth, screenHeight, "Catch_Fruits_Test1.exe");
 
     SetTargetFPS(144);
 
     // Audio initialization
     InitAudioDevice();
-    music = LoadMusicStream("textures_audios/JoJo_s Bizarre Adventure_ Diamond is Unbreakable OST - Italian Restaurant.mp3");
-    PlayMusicStream(music);
-    SetMusicVolume(music, 0.25f);
-    bruh = LoadSound("textures_audios/vineboom.ogg");
-    SetSoundVolume(bruh, 0.25f);
-    chatSound = LoadSound("textures_audios/clink.ogg");
-    SetSoundVolume(chatSound, 0.25f);
+    menuMusic = LoadMusicStream("audios/BGM/menuSoundtrack.ogg");
+    PlayMusicStream(menuMusic);
+    SetMusicVolume(menuMusic, 0.5f);
 
     // Load custom font
-    Font customFont = LoadFontEx("fonts/Tickerpixel-monoitalic.otf", 32, 0, 0);
+    Font customFont = LoadFontEx("fonts/BitPotion.ttf", 32, 0, 0);
 
-    // Menu Pic initialization
-    menuPic = LoadTexture("textures_audios/pfp.png");
+    // Menu Background initialization
+    menuBackground = LoadTexture("textures_audios/pfp.png");
 
-    const char *titleText = "Test";
-    const char *textMenu = "press any key to start";
+    const char *titleText = "Catch the Fruits";
     
     // Array of chat texts
     const char *chatTexts[] = {
-        "~wsg chat.",
-        "~Guys, am I cool or cool?",
-        "~Jarvis, unpiss my pants.",
-        "~You guys should play deepwoken ong.",
-        "~Soo...2024 actually wasn't my year.",
-        "~It's official, I've gone insane.",
-        "~I feel so skibidi.",
-        "~I just lost my dawg.",
-        "~Those who know...",
-        "~I understand it now.",
-        "~The voices in my walls are telling me I get 0 bitches."
+        "This is merely a test. Things might get weird",
+        "Children's game is very chill and fun.",
+        "When you hear a whisper in the game, open your earphones, and listen carefully",
+        "You should try Jason's Roullete",
+        "You can't escape from this game.",
+        "You should try Adrian's Tetris",
+        "The more you progress, the more it's not a game anymore.",
+        "I love her",
+        "I shouldn't have created it.",
+        "Minecraft is a goated game",
+        "If you see this, it's too late.",
+        "Hello miss, if you see this, please give me HD hehe",
+        "What have I done?"
     };
     int chatTextCount = sizeof(chatTexts) / sizeof(chatTexts[0]);
     const char *currentChatText = chatTexts[0]; // Initial chat text
@@ -65,12 +68,14 @@ int main()
     int textTitleWidth = textTitleSize.x;
     int textTitleHeight = textTitleSize.y;
 
+    // Menu text
+    const char *textMenu = "Menu";
+
     // Menu text measurements
     Vector2 textMenuSize = MeasureTextEx(customFont, textMenu, fontSize, 1);
     int textMenuWidth = textMenuSize.x;
     int textMenuHeight = textMenuSize.y;
 
-    bool startMenu = true;
     bool showChat = false; // State variable to track if the chat box should be displayed
     float elapsedTime = 0.0f;
     float scale  = 0.25f;
@@ -79,27 +84,28 @@ int main()
 
     srand(time(NULL)); // Seed the random number generator
 
-    // Variables for Russian Roulette game
+    // Variables for the game
     bool gameStarted = false;
     bool gameOver = false;
-    bool playerAlive = true;
-    bool aiAlive = true;
-    bool playerTurn = true; // Flag to determine whose turn it is
-    int bulletPosition = rand() % 6;
+    int fruitsCaught = 0;
+    int fruitsMissed = 0;
+    bool darkTwist = false;
+
+    GameState gameState = MENU;
 
     while (!WindowShouldClose())
     {
-        UpdateMusicStream(music);
+        UpdateMusicStream(menuMusic);
         elapsedTime += GetFrameTime();
 
-        if (startMenu)
+        if (gameState == MENU)
         {
             // Check if any key is pressed except ESC
             for (int key = 32; key < 349; key++) // Key codes range from 32 to 348
             {
                 if (key != KEY_ESCAPE && IsKeyPressed(key))
                 {
-                    startMenu = false; // Transition to game state
+                    gameState = GAME; // Transition to game state
                     break;
                 }
             }
@@ -108,15 +114,15 @@ int main()
             {
                 Vector2 mousePosition = GetMousePosition();
                 Rectangle menuPicBounds = {
-                    (screenWidth - menuPic.width * scale) / 4, // Move to the left
-                    (screenHeight - textTitleHeight) / 2 - menuPic.height * scale - (yOffset + 50),
-                    menuPic.width * scale,
-                    menuPic.height * scale
+                    (screenWidth - menuBackground.width * scale) / 4, // Move to the left
+                    (screenHeight - textTitleHeight) / 2 - menuBackground.height * scale - (yOffset + 50),
+                    menuBackground.width * scale,
+                    menuBackground.height * scale
                 };
 
                 if (CheckCollisionPointRec(mousePosition, menuPicBounds))
                 {
-                    PlaySound(bruh); // Play the sound when the menuPic is clicked
+                    // PlaySound(); // Play the sound when the menuBackground is clicked
                     showChat = true; // Show the chat box
 
                     // Select a random chat text
@@ -129,7 +135,7 @@ int main()
                 }
                 else
                 {
-                    startMenu = false;
+                    gameState = GAME;
                 }
             }
         }
@@ -144,7 +150,7 @@ int main()
                 if (charIndex < strlen(currentChatText))
                 {
                     charIndex++;
-                    PlaySound(chatSound); // Play the chat sound effect
+                    // PlaySound(chatSound); // Play the chat sound effect
                 }
             }
         }
@@ -153,12 +159,12 @@ int main()
 
         ClearBackground(RAYWHITE);
 
-        if (startMenu)
+        if (gameState == MENU)
         {
             // Draw menu picture above the title, scaled down
-            float menuPicX = (screenWidth - menuPic.width * scale) / 4; // Move to the left
-            float menuPicY = (screenHeight - textTitleHeight) / 2 - menuPic.height * scale - (yOffset + 50);
-            DrawTextureEx(menuPic, (Vector2){ menuPicX, menuPicY }, 0.0f, scale, WHITE);
+            float menuPicX = (screenWidth - menuBackground.width * scale) / 4; // Move to the left
+            float menuPicY = (screenHeight - textTitleHeight) / 2 - menuBackground.height * scale - (yOffset + 50);
+            DrawTextureEx(menuBackground, (Vector2){ menuPicX, menuPicY }, 0.0f, scale, WHITE);
 
             // Draw title text with wave effect
             int titleLength = strlen(titleText);
@@ -171,97 +177,111 @@ int main()
                            fontTitleSize, 1, BLACK);
             }
 
-            // Draw menu text centered below the title
-            DrawTextEx(customFont, textMenu, (Vector2){ (screenWidth - textMenuWidth) / 2, (screenHeight - textMenuHeight) / 2 + yOffset }, fontSize, 1, BLACK);
+            // Draw menu buttons
+            Rectangle playButton = { screenWidth / 2 - 100, screenHeight / 2, 200, 50 };
+            Rectangle infoButton = { screenWidth / 2 - 100, screenHeight / 2 + 60, 200, 50 };
+            Rectangle quitButton = { screenWidth / 2 - 100, screenHeight / 2 + 120, 200, 50 };
+
+            DrawRectangleRec(playButton, LIGHTGRAY);
+            DrawRectangleRec(infoButton, LIGHTGRAY);
+            DrawRectangleRec(quitButton, LIGHTGRAY);
+
+            DrawTextEx(customFont, "Play", (Vector2){ playButton.x + 70, playButton.y + 10 }, 30, 1, BLACK);
+            DrawTextEx(customFont, "Info", (Vector2){ infoButton.x + 70, infoButton.y + 10 }, 30, 1, BLACK);
+            DrawTextEx(customFont, "Quit", (Vector2){ quitButton.x + 70, quitButton.y + 10 }, 30, 1, BLACK);
+
+            // Check button clicks
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            {
+                Vector2 mousePosition = GetMousePosition();
+                if (CheckCollisionPointRec(mousePosition, playButton))
+                {
+                    gameState = GAME;
+                }
+                else if (CheckCollisionPointRec(mousePosition, infoButton))
+                {
+                    gameState = INFO;
+                }
+                else if (CheckCollisionPointRec(mousePosition, quitButton))
+                {
+                    CloseWindow();
+                }
+            }
 
             // Draw chat text if the chat box is active
             if (showChat)
             {
-                float chatTextY = menuPicY + (menuPic.height * scale / 2) - (fontChatSize / 2);
+                float chatTextY = menuPicY + (menuBackground.height * scale / 2) - (fontChatSize / 2);
                 DrawTextEx(customFont, TextSubtext(currentChatText, 0, charIndex), 
-                           (Vector2){ menuPicX + menuPic.width * scale + 10, chatTextY }, 
+                           (Vector2){ menuPicX + menuBackground.width * scale + 10, chatTextY }, 
                            fontChatSize, 1, BLACK);
             }
         }
-        else if (!gameStarted)
+        else if (gameState == GAME)
         {
-            // Start the Russian Roulette game
-            if (IsKeyPressed(KEY_SPACE))
+            // Game logic for catching fruits
+            if (!gameStarted)
             {
-                gameStarted = true;
-            }
-        }
-        else if (!gameOver)
-        {
-            if (playerTurn)
-            {
-                DrawTextEx(customFont, "Press SPACE to pull the trigger", (Vector2){ screenWidth / 2 - MeasureTextEx(customFont, "Press SPACE to pull the trigger", 20, 1).x / 2, screenHeight / 2 - 10 }, 20, 1, BLACK);
-
                 if (IsKeyPressed(KEY_SPACE))
                 {
-                    int triggerPull = rand() % 6;
-                    if (triggerPull == bulletPosition)
-                    {
-                        playerAlive = false;
-                        gameOver = true;
-                        PlaySound(bruh);
-                    }
-                    else
-                    {
-                        PlaySound(chatSound);
-                        playerTurn = false; // Switch to AI's turn
-                    }
+                    gameStarted = true;
                 }
+            }
+            else if (!gameOver)
+            {
+                // Game logic for catching fruits
+                // If the player misses more than 7 fruits, trigger the dark twist
+                if (fruitsMissed >= 7)
+                {
+                    darkTwist = true;
+                    gameOver = true;
+                }
+
+                // Update game logic here
+                // ...
+
+                // Example: If the player catches a fruit
+                // if (/* condition for catching a fruit */)
+                // {
+                //     fruitsCaught++;
+                // }
+                // // Example: If the player misses a fruit
+                // else if (/* condition for missing a fruit */)
+                // {
+                //     fruitsMissed++;
+                // }
             }
             else
             {
-                DrawTextEx(customFont, "AI's turn...", (Vector2){ screenWidth / 2 - MeasureTextEx(customFont, "AI's turn...", 20, 1).x / 2, screenHeight / 2 - 10 }, 20, 1, BLACK);
-
-                // Simulate AI pulling the trigger after a short delay
-                if (elapsedTime > 1.0f)
+                if (darkTwist)
                 {
-                    int triggerPull = rand() % 6;
-                    if (triggerPull == bulletPosition)
-                    {
-                        aiAlive = false;
-                        gameOver = true;
-                        PlaySound(bruh);
-                    }
-                    else
-                    {
-                        PlaySound(chatSound);
-                        playerTurn = true; // Switch back to player's turn
-                    }
-                    elapsedTime = 0.0f; // Reset elapsed time for next turn
+                    DrawTextEx(customFont, "The game has taken a dark twist...", (Vector2){ screenWidth / 2 - MeasureTextEx(customFont, "The game has taken a dark twist...", 32, 1).x / 2, screenHeight / 2 - 10 }, 32, 1, BLACK);
+                }
+                else
+                {
+                    DrawTextEx(customFont, "Game Over!", (Vector2){ screenWidth / 2 - MeasureTextEx(customFont, "Game Over!", 32, 1).x / 2, screenHeight / 2 - 10 }, 32, 1, BLACK);
+                }
+
+                DrawTextEx(customFont, "Press R to restart", (Vector2){ screenWidth / 2 - MeasureTextEx(customFont, "Press R to restart", 20, 1).x / 2, screenHeight / 2 + 40 }, 20, 1, BLACK);
+
+                if (IsKeyPressed(KEY_R))
+                {
+                    gameStarted = false;
+                    gameOver = false;
+                    fruitsCaught = 0;
+                    fruitsMissed = 0;
+                    darkTwist = false;
                 }
             }
         }
-        else
+        else if (gameState == INFO)
         {
-            if (playerAlive)
-            {
-                DrawTextEx(customFont, "You survived!", (Vector2){ screenWidth / 2 - MeasureTextEx(customFont, "You survived!", 32, 1).x / 2, screenHeight / 2 - 10 }, 32, 1, BLACK);
-            }
-            else
-            {
-                DrawTextEx(customFont, "You died!", (Vector2){ screenWidth / 2 - MeasureTextEx(customFont, "You died!", 32, 1).x / 2, screenHeight / 2 - 10 }, 32, 1, BLACK);
-            }
+            DrawTextEx(customFont, "Information about the game", (Vector2){ screenWidth / 2 - MeasureTextEx(customFont, "Information about the game", 32, 1).x / 2, screenHeight / 2 - 10 }, 32, 1, BLACK);
+            DrawTextEx(customFont, "Press ESC to return to menu", (Vector2){ screenWidth / 2 - MeasureTextEx(customFont, "Press ESC to return to menu", 20, 1).x / 2, screenHeight / 2 + 40 }, 20, 1, BLACK);
 
-            if (!aiAlive)
+            if (IsKeyPressed(KEY_ESCAPE))
             {
-                DrawTextEx(customFont, "AI died!", (Vector2){ screenWidth / 2 - MeasureTextEx(customFont, "AI died!", 32, 1).x / 2, screenHeight / 2 + 40 }, 32, 1, BLACK);
-            }
-
-            DrawTextEx(customFont, "Press R to restart", (Vector2){ screenWidth / 2 - MeasureTextEx(customFont, "Press R to restart", 20, 1).x / 2, screenHeight / 2 + 80 }, 20, 1, BLACK);
-
-            if (IsKeyPressed(KEY_R))
-            {
-                gameStarted = false;
-                gameOver = false;
-                playerAlive = true;
-                aiAlive = true;
-                playerTurn = true;
-                bulletPosition = rand() % 6;
+                gameState = MENU;
             }
         }
 
@@ -269,11 +289,9 @@ int main()
     }
 
     // De-Initialization
-    StopMusicStream(music);
+    StopMusicStream(menuMusic);
     UnloadFont(customFont);
-    UnloadTexture(menuPic);
-    UnloadSound(bruh);
-    UnloadSound(chatSound);
+    UnloadTexture(menuBackground);
     CloseAudioDevice(); // Close the audio device
     CloseWindow();
 
